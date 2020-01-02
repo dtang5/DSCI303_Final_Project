a418cd527c48b943a44f26f22ca03f09f147901f
Daniel Tang <dt19@rice.edu>
2020-01-02 14:31:05 -0800

    added whitepaper tab

--- Front_End/app.py
+++ Front_End/app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, send_file
 
 from sentimentAnalysis import tfidf_predict_rating_logistic, count_predict_rating_svm, tfidf_predict_rating_svm
 from statistics import mean
-
 
 app = Flask(__name__)
 
@@ -18,19 +16,37 @@ def start():
     return render_template('start.html')
 
 
+@app.route('/whitepaper')
+def whitepaper():
+    return render_template('whitepaper.html')
+
+
+@app.route('/return-files/')
+def return_files():
+    try:
+        return send_file('Sentiment_Analysis_Yelp_Reviews_Paper.pdf',
+                         attachment_filename='whitepaper.pdf')
+    except Exception as e:
+        return str(e)
+
+
 @app.route('/startLogisticTFIDF', methods=['POST'])
 def start_logistic_post():
     mail = request.form['mail']
     unprocessed_review = [mail]
-    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(int(tfidf_predict_rating_logistic(unprocessed_review)[0])) + " star! :)"
+    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(
+        int(tfidf_predict_rating_logistic(unprocessed_review)[0])) + " star! :)"
     return render_template('rating.html', review=mail, rating=predicted_rating)
+
 
 @app.route('/startSVMCount', methods=['POST'])
 def start_SVM_post():
     mail = request.form['mail']
     unprocessed_review = [mail]
-    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(int(count_predict_rating_svm(unprocessed_review)[0])) + " star! :)"
+    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(
+        int(count_predict_rating_svm(unprocessed_review)[0])) + " star! :)"
     return render_template('rating.html', review=mail, rating=predicted_rating)
+
 
 # @app.route('/startRFTFIDF', methods=['POST'])
 # def start_RF_TFIDF_post():
@@ -43,14 +59,17 @@ def start_SVM_post():
 def start_SVM_TFIDF_post():
     mail = request.form['mail']
     unprocessed_review = [mail]
-    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(int(tfidf_predict_rating_svm(unprocessed_review)[0])) + " star! :)"
+    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(
+        int(tfidf_predict_rating_svm(unprocessed_review)[0])) + " star! :)"
     return render_template('rating.html', review=mail, rating=predicted_rating)
+
 
 @app.route('/startAverage', methods=['POST'])
 def start_avg_post():
     mail = request.form['mail']
     unprocessed_review = [mail]
-    r = mean([tfidf_predict_rating_svm(unprocessed_review)[0], tfidf_predict_rating_logistic(unprocessed_review)[0], count_predict_rating_svm(unprocessed_review)[0]])
-    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(round(r, 2)) + " star! :)"
+    r = mean([tfidf_predict_rating_svm(unprocessed_review)[0], tfidf_predict_rating_logistic(unprocessed_review)[0],
+              count_predict_rating_svm(unprocessed_review)[0]])
+    predicted_rating = "Based on your review, your rating for this merchant or service should be around a " + str(
+        round(r, 2)) + " star! :)"
     return render_template('rating.html', review=mail, rating=predicted_rating)
-
